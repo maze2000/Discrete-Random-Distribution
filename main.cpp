0a0ee651bcc15bd864a47fd01d430963fbdaf2b8
maze2000 <matthias-rein@web.de>
2015-06-19 15:15:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int table[255*size][4];
 unsigned int t[4];
 const int x = 255;
 int K[size];
-int V[size];
+double V[size];
 
 void initiate(std::pair<int, unsigned int>* a)
 {
@@ -39,38 +39,57 @@ void initiate(std::pair<int, unsigned int>* a)
     t[1] = t[2] + (t[1] << 8);
     t[0] = t[1] + t[0];
     
-   //std::cout << table[103][0] << std::endl;
-    
-    /*
-    int q = 0;
-    std::cin >> q;
-    for(int j = -1; j < 10; j++){
-      counter = 0;
-      for(int i = 0; i < 255*size; i++) {
-	 if (table[i][q]==j)
-	  counter++;
-      }
-      	std::cout << counter << std::endl; 
-      
-    }*/
 }
 
-/*
+void initiate(std::pair<int, float>* a) {
+  
+  std::pair<int, unsigned int> b[size];
+  for(int i = 0; i < size; i++) {
+    b[i].first = a[i].first;
+    b[i].second = a[i].second * INT_MAX;
+  }
+  initiate(b);
+}
+
+void dispVK() {
+  for(int i = 0; i < size; i++) {
+    std::cout << "V[" << i << "]: " << V[i] << std::endl;
+  }
+  std::cout << std::endl;
+  for(int i = 0; i < size; i++) {
+    std::cout << "K[" << i << "]: " << K[i] << std::endl;
+  }
+  std::cout << std::endl;
+}
+
 void initiate2(std::pair<int, unsigned int>* a)
 {
     int counter = 0;
     for(int i = 0; i < size; i++)
     {
-        for(int j = 0; j < (int) ((a[i].second & (x << (24))) >> 24); j++)
+        for(int j = 0; j < (int) ((a[i].second >> 24) & x); j++)
         {
-                table[counter][3] = a[i].first;
+                table[counter][3] = a[i].first-1;
                 counter++;
         }
     }
     for(; counter < 255*size; counter++)
         table[counter][3] = -1;
-
-    double avg = 1/size;
+    
+    double b[size];
+    double sum = 0;
+    for(int i = 0; i < size; i++)
+    {
+      b[i] = ((double) a[i].second) / INT_MAX;
+      sum += b[i];
+    }
+    
+    for (int i = 0; i < size; i++)
+      std::cout << "b[" << i << "]: " << b[i] << std::endl;
+    
+    std::cout << std::endl;
+    
+    double avg = 1.0/size;
     int max = 0;
     int min = 0;
 
@@ -79,24 +98,25 @@ void initiate2(std::pair<int, unsigned int>* a)
         K[i] = i;
         V[i] = (i+1) * avg;
     }
-
+    dispVK();
     for(int i = 0; i < size; i++)
     {
-        for(int j = 1; j < size; j++)
+        for(int j = 0; j < size; j++)
         {
-            if (a[j].second > a[max].second)
+            if (b[j] >= b[max])
                 max = j;
-            if (a[j].second < a[max].second)
+            if (b[j] < b[min])
                 min = j;
         }
         K[min] = max;
-        V[min] = (min-1) * avg + a[min].second;
-        a[max].second = a[max].second - (avg-a[min].second);
-        a[min].second = avg;
+        V[min] = (min) * avg + b[min];
+        b[max] = b[max] - (avg-b[min]);
+        b[min] = avg;
+	
+	dispVK();
+  
     }
-
 }
-*/
 
 int generate(unsigned int random) {
   
@@ -108,40 +128,54 @@ int generate(unsigned int random) {
         return table[(random-t[2]) >> 8][1];
     if(random < t[0])
         return table[random - t[1]][0];
-    std::cout << "ERR" << std::endl;
+    //std::cout << "ERR" << std::endl;
     return 0;
     
 }
-/*
+  
 int generate2(unsigned int random) {
-
     if(table[random&255][3] >= 0)
         return table[random&255][3];
-    double i = ((double) random)/pow(2,32);
-    int j = 5*i;
-    //std::cout << j << std::endl;
-
-    if (random < V[j])
+    
+    double i = ((double) random)/INT_MAX;
+    int j = size*i;
+    if (i < V[j]) {
         return j;
+    }
     return K[j];
 }
-*/
+
 int main() {
 
     std::pair<int,unsigned int> a[size];
     for(int i = 0; i < size; i++) {
         a[i] = std::make_pair(i+1,INT_MAX/size);
     }
+    
+    std::pair<int,float> a2[size];
+    for(int i = 0; i < size; i++) {
+        a2[i] = std::make_pair(i+1,1.0/size);
+    }
+    
     /*a[0].second = INT_MAX/size * 1/2;
     a[1].second = INT_MAX/size * 1/2;
-    a[2].second = INT_MAX/size;
+    a[2].second = INT_MAX/size * 1;
     a[3].second = INT_MAX/size * 2;*/
-    initiate(a);
+    
+    initiate2(a);	
     int random;
     int sum = 0;
-    for(int i = 0; i < 100000; i++) {
+    for(int i = 0; i < 10000000; i++) {
+      random = generate2(rand());
+      sum += random;
+    }
+    std::cout << sum << std::endl;
+    initiate(a);
+    sum = 0;
+    for(int i = 0; i < 10000000; i++) {
       random = generate(rand());
       sum += random;
     }
     std::cout << sum << std::endl;
+
 }
