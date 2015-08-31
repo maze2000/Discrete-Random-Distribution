6e240104d33ae1fb6b60aa7c5e35c307f92ffafe
maze2000 <matthias-rein@web.de>
2015-08-31 13:39:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,48 +4,144 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_cdf.h>
+#include <gsl/gsl_rng.h>
+#include <RandomLib/Random.hpp>
+#include <RandomLib/RandomSelect.hpp>
+
+#include <chrono>
+#include <random>
 
 std::string s("");
+
+void testBoost() {
+  
+  int size = 10000;
+  int max = 100000;
+  boost::mt19937 gen;
+  std::vector<double> a;
+  clock_t startTime;
+  
+  for(int i = 0; i < 10; i++) {
+    
+  for(int i = 0; i<size; i++) {
+    a.push_back(rand());
+  }
+    
+  boost::random::discrete_distribution<> dist(a.begin(), a.end());
+
+  std::cout << "\t Boost: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
+  startTime = clock();  
+  for(int i = 0; i < max; i++) {
+    dist(gen);
+  }
+
+  std::cout << "\t\t Generate: " << clock() - startTime << std::endl << std::endl;
+  
+  }
+  
+  for(int i = 0; i<size; i++) {
+    a[i] = 1.0/size;
+  }
+  
+  startTime = clock();
+  
+  boost::random::discrete_distribution<> dist2(a.begin(), a.end());
+
+  std::cout << "\t Boost: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
+  startTime = clock();  
+  for(int i = 0; i < max; i++) {
+    dist2(gen);
+  }
+
+  std::cout << "\t\t Generate: " << clock() - startTime << std::endl << std::endl;
+
+}
 
 void test(int max, int size) {
   
   std::vector<double> a;
-  //double b[size];
+  std::vector<int> b;
+  double d[size];
+
   for(int i = 0; i<size; i++) {
     a.push_back(1.0/size);
-    //b[i] = 1.0/size;
+    b.push_back(1);
+    d[i] = rand();;
   }
-  //double* c = &a[0];
   
-  s += std::to_string(max);
-  s += "\t";
+  //s += std::to_string(max);
+  //s += "\t";
   s += std::to_string(size);
   s += "\t";
   
   int sum = 0;
   
   std::cout << std::endl << "Max: " << max << ";\t Size: " << size << std::endl << std::endl;
+    
+  boost::mt19937 gen;
+  
+  
+  const gsl_rng_type * T;
+  gsl_rng * r;
+
+  gsl_rng_env_setup();
+
+  T = gsl_rng_default;
+  r = gsl_rng_alloc(T);
+  
+  RandomLib::Random r2; 
   
   sum = 0;
   clock_t startTime = clock(); 
   
   
-  boost::mt19937 gen;
   
-  double probabilities[] = {
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 
-    0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01
-  };
+  //RandomLib
   
-  boost::random::discrete_distribution<> dist(probabilities);
+  sum = 0;
+  startTime = clock();
+  RandomLib::RandomSelect<int> sel(b.begin(), b.end());
+  s += std::to_string(clock() - startTime);
+  s += "\t";
+  std::cout << "\t RandomLib: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
+  startTime = clock();  
+  for(int i = 0; i < max; i++) {
+    sum += sel(r2);
+  }
+  s += std::to_string(clock() - startTime);
+  s += "\t";
+  std::cout << "\t\t Generate: " << clock() - startTime << std::endl << "\t\t Sum: " << sum << std::endl;
+  
+  
+  //gnu science
+  
+  sum = 0;
+  startTime = clock();
+  gsl_ran_discrete_t * table = gsl_ran_discrete_preproc(size, d);
+  s += std::to_string(clock() - startTime);
+  s += "\t";
+  std::cout << "\t Gnu Science: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
+  startTime = clock();  
+  for(int i = 0; i < max; i++) {
+    sum += gsl_ran_discrete(r, table);
+  }
+  s += std::to_string(clock() - startTime);
+  s += "\t";
+  std::cout << "\t\t Generate: " << clock() - startTime << std::endl << "\t\t Sum: " << sum << std::endl;
+  
+  gsl_rng_free(r);
+
+  
+  //boost
+  
+  sum = 0;
+  startTime = clock();
+  boost::random::discrete_distribution<> dist(a.begin(), a.end());
+  //boost::random::discrete_distribution<> dist(b, b+size);
+  s += std::to_string(clock() - startTime);
+  s += "\t";
   std::cout << "\t Boost: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
   startTime = clock();  
   for(int i = 0; i < max; i++) {
@@ -55,9 +151,31 @@ void test(int max, int size) {
   s += "\t";
   std::cout << "\t\t Generate: " << clock() - startTime << std::endl << "\t\t Sum: " << sum << std::endl;
   
+  
+  //std::dsicrete_distribution
+  
   sum = 0;
-  startTime = clock(); 
+  startTime = clock();
+  std::discrete_distribution<int> std_dist(b.begin(), b.end());
+  s += std::to_string(clock() - startTime);
+  s += "\t";
+  std::cout << "\t std::discrete_distribution: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
+  startTime = clock();  
+  for(int i = 0; i < max; i++) {
+    sum += std_dist(gen);
+  }
+  s += std::to_string(clock() - startTime);
+  s += "\t";
+  std::cout << "\t\t Generate: " << clock() - startTime << std::endl << "\t\t Sum: " << sum << std::endl;
+  
+  /*
+  //table-lookup
+  
+  sum = 0;
+  startTime = clock();
   random_distribution q(a);
+  s += std::to_string(clock() - startTime);
+  s += "\t";
   std::cout << "\t Method 1: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
   startTime = clock();  
   for(int i = 0; i < max; i++) {
@@ -66,10 +184,16 @@ void test(int max, int size) {
   s += std::to_string(clock() - startTime);
   s += "\t";
   std::cout << "\t\t Generate: " << clock() - startTime << std::endl << "\t\t Sum: " << sum << std::endl;
+  //std::cout << "\t\t i0: " << q.i0 << "\t i1: " << q.i1 <<  "\t i2: " << q.i2 <<  "\t i3: " << q.i3 << std::endl;
+
+  
+  //table-lookup + histogram
   
   sum = 0;
   startTime = clock();
   random_distribution q2(a,2);
+  s += std::to_string(clock() - startTime);
+  s += "\t";
   std::cout << "\t Method 2: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
   startTime = clock();  
   for(int i = 0; i < max; i++) {
@@ -79,9 +203,14 @@ void test(int max, int size) {
   s += "\t";
   std::cout << "\t\t Generate: " << clock() - startTime << std::endl << "\t\t Sum: " << sum << std::endl;
     
+  
+  //binary search
+  
   sum = 0;
   startTime = clock();
   random_distribution q3(a,3);
+  s += std::to_string(clock() - startTime);
+  s += "\t";
   std::cout << "\t Method 3: " << std::endl << "\t\t Initiate: " << clock() - startTime << std::endl;
   startTime = clock();  
   for(int i = 0; i < max; i++) {
@@ -90,41 +219,20 @@ void test(int max, int size) {
   s += std::to_string(clock() - startTime);
   s += "\t";
   std::cout << "\t\t Generate: " << clock() - startTime << std::endl << "\t\t Sum: " << sum << std::endl;  
-  
+  */
   s += "\n";
 }
 
 int main() {
   
-  test(100000, 100); /*
-  test(100000, 1000);
-  test(100000, 2000);
-  test(100000, 3000);
-  test(100000, 4000);
-  test(100000, 5000);
-  test(100000, 6000);
-  test(100000, 7000);
-  test(100000, 8000);
-  test(100000, 9000);
-  test(100000, 10000);
-  test(100000, 11000);
-  test(100000, 12000);
-  test(100000, 13000);
-  test(100000, 14000);
-  test(100000, 15000);
-  test(100000, 16000);
-  test(100000, 17000);
-  test(100000, 18000);
-  test(100000, 19000); 
-  //test(100000, 100000);
   
-  //test(1000000, 10000);
-  //test(10000000, 10000);
-  //test(100000000, 10000);
+  for(int i = 10000; i <= 1000000; i += 10000)
+    test(100000, i);
   
   std::ofstream output("data.txt");
   output << s;
-  output.close(); */
+  output.close();
+  //testBoost();
   
   return 0;
 }
